22578d27e39fc7369dfb7a1ab09194042e9f1c52
Hager Elsayed <Hasanieenh@mobilenoc.mobi>
2023-07-20 22:59:59 +0300

    Initial Commit

---
+++ YassirChallengeTask/YassirChallengeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  YassirChallengeTaskApp.swift
+//  YassirChallengeTask
+//
+//  Created by Hager Elsayed on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct YassirChallengeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
