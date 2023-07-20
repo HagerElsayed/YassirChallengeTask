db90fb7257797046f8d5553df51158f3b4d8c5d5
Hager Elsayed <Hasanieenh@mobilenoc.mobi>
2023-07-20 23:31:33 +0300

    feature: handle App Folders Architecture

---
+++ YassirChallengeTask/Core/Home/View/MovieHomeView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieHomeView.swift
+//  YassirChallengeTask
+//
+//  Created by Hager Elsayed on 20/07/2023.
+//
+
+import SwiftUI
+
+struct MovieHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieHomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieHomeView()
+    }
+}
---
+++ YassirChallengeTask/Core/Home/View/MovieItemView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieItemView.swift
+//  YassirChallengeTask
+//
+//  Created by Hager Elsayed on 20/07/2023.
+//
+
+import SwiftUI
+
+struct MovieItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieItemView()
+    }
+}
